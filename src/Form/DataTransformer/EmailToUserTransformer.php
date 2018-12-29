a30f6dd7b555d356e49aa4b0de163c1705d7ff94
petrero <petrero@gmail.com>
2018-12-29 13:26:40 -0600

    Copy code from previous branch

---
+++ src/Form/DataTransformer/EmailToUserTransformer.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Form\DataTransformer;
+
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class EmailToUserTransformer implements  DataTransformerInterface {
+	/**
+	 * @var UserRepository
+	 */
+	private $userRepo;
+
+	public function __construct(UserRepository $userRepo) {
+		$this->userRepo = $userRepo;
+	}
+
+	public function transform($value) {
+		if(null === $value){
+			return '';
+		}
+
+		if(!$value instanceof User){
+			throw new \LogicException('The UserSelectTextType can only be used with User objects.');
+		}
+
+		return $value->getEmail();
+	}
+
+	public function reverseTransform($value) {
+		$user = $this->userRepo->findOneBy(['email' => $value]);
+
+		if(!$user){
+			throw new TransformationFailedException(sprintf(
+				'No user found with email "%s"',
+				$value
+			));
+		}
+
+		return $user;
+	}
+
+}
