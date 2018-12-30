cf42ebf02bee9e25348eeeb084e9a295968f63b3
petrero <petrero@gmail.com>
2018-12-30 10:54:55 -0600

    Copy code from previous branch

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -9,6 +9,8 @@ use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
@@ -25,6 +27,7 @@ class Article {
 
   /**
    * @ORM\Column(type="string", length=255)
+   * @Assert\NotBlank(message="Get creative and think of a title!")
    */
   private $title;
 
@@ -220,4 +223,21 @@ class Article {
 
     return $this;
   }
+
+	public function isPublished(): bool{
+		return $this->publishedAt !== null;
+	}
+
+	/**
+	 * @Assert\Callback
+	 */
+	public function validate(ExecutionContextInterface $context, $payload) {
+		if (stripos($this->getTitle(), 'the borg') !== false){
+			$context
+				->buildViolation('\'Um.. the Bork kinda makes us nervous\'')
+				->atPath('title')
+				->addViolation()
+			;
+		}
+	}
 }
