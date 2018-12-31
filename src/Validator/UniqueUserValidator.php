6fd97fbc76f8059b8cd3eddb1092ad209da7c92b
petrero <petrero@gmail.com>
2018-12-31 04:56:06 -0600

    Run: bin/console make:validator
    Unfortunately, you can't use the @UniqueEntity() validation constraint above a class that is not an entity: it's just a known limitation. But, fortunately, this gives us the perfect excuse to create a custom validation constraint! Woo!
    
    When you can't find a built-in validation constraint that does what you need, the next thing to try is the @Assert\Callback constraint. We use this in the Article class. But, it has one limitation: because the method lives inside an entity class - we do not have access to any services. In our case, in order to know whether or not the email is taken yet, we need to make a query and so we do need to access a service.
    
    Generating the Constraint Validator
    
    When that's your situation, it's time for a custom validation constraint. They're awesome anyways and we're going to cheat! Find your terminal and run:
      php bin/console make:validator
    
    Call the class, how about, UniqueUser. Oh, this created two classes: UniqueUser and UniqueUserValidator. You'll find these inside a new Validator/ directory. Look at UniqueUser first: it's basically a dumb configuration object. This will be the class we use for our annotation.
    
    The actual validation is handled by UniqueUserValidator: Symfony will pass it the value being validated and a Constraint object - which will be that UniqueUser object we just saw. We'll use it to read some options to help us get our job done. For example, in the generated code, it reads the message property from the  and sets that as the validation error. That's literally reading this public  property from UniqueUser.

---
+++ src/Validator/UniqueUserValidator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class UniqueUserValidator extends ConstraintValidator {
+  public function validate($value, Constraint $constraint) {
+    /* @var $constraint App\Validator\UniqueUser */
+
+    $this->context->buildViolation($constraint->message)
+      ->setParameter('{{ value }}', $value)
+      ->addViolation();
+  }
+}
