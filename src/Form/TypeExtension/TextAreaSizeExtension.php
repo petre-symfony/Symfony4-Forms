a6b572e6ecf51e456e3dc5d01828d07a5bdeb347
petrero <petrero@gmail.com>
2018-12-31 10:01:27 -0600

    Copy code from previous branch

---
+++ src/Form/TypeExtension/TextAreaSizeExtension.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Form\TypeExtension;
+
+
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormTypeExtensionInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TextAreaSizeExtension implements FormTypeExtensionInterface {
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		// TODO: Implement buildForm() method.
+	}
+
+	public function buildView(FormView $view, FormInterface $form, array $options) {
+		$view->vars['attr']['rows'] = $options['rows'];
+	}
+
+	public function finishView(FormView $view, FormInterface $form, array $options) {
+		// TODO: Implement finishView() method.
+	}
+
+	public function configureOptions(OptionsResolver $resolver) {
+		$resolver->setDefaults([
+			'rows' => 10
+		]);
+	}
+
+	public function getExtendedType() {
+		return TextareaType::class;
+	}
+
+}
---
+++ src/Validator/UniqueUserValidator.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Validator;
+
+use App\Repository\UserRepository;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class UniqueUserValidator extends ConstraintValidator {
+	/**
+	 * @var UserRepository
+	 */
+	private $userRepo;
+
+	public function __construct(UserRepository $userRepo) {
+		$this->userRepo = $userRepo;
+	}
+
+	public function validate($value, Constraint $constraint) {
+    /* @var $constraint App\Validator\UniqueUser */
+
+    $existingUser = $this->userRepo->findOneBy([
+    	'email' => $value
+    ]);
+
+    if(!$existingUser){
+    	return;
+    }
+    
+    $this->context->buildViolation($constraint->message)
+      ->addViolation();
+  }
+}
