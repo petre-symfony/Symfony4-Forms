8abd2230c37803b1ded882a492921ab2379c8104
petrero <petrero@gmail.com>
2018-12-31 02:41:22 -0600

    Copy code from previous branch

---
+++ public/js/algolia-autocomplete.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+	$('.js-user-autocomplete').each(function () {
+		var autocompleteURL = $(this).data('autocomplete-url');
+		$(this).autocomplete({hint: false}, [{
+			source: function (query, cb) {
+				$.ajax({
+					url: autocompleteURL+'?query='+query
+				})
+				.then(function (data) {
+					cb(data.users);
+				})
+			},
+			displayKey: 'email',
+			debounce: 500
+		}])
+	});
+
+})
