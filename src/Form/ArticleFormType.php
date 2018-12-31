b273ace84893d9d7264ef58b0fcfb767e926345a
petrero <petrero@gmail.com>
2018-12-31 07:47:42 -0600

    Copy code from previous branch

---
+++ src/Form/ArticleFormType.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleFormType extends AbstractType {
+	/**
+	 * @var UserRepository
+	 */
+	private $userRepo;
+
+	public function __construct(UserRepository $userRepo) {
+		$this->userRepo = $userRepo;
+	}
+
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$article = $options['data'] ?? null;
+		$isEdit = $article && $article->getId();
+
+		$builder
+			->add('title', TextType::class, [
+				'help' => 'Choose something catchy!'
+			])
+			->add('content', null, [
+				'rows' => 15
+			])
+			->add('author', UserSelectTextType::class, [
+				'disabled' => $isEdit
+			])
+			->add('location', ChoiceType::class, [
+				'placeholder' => 'Choose a location',
+				'choices' => [
+					'The Solar System' => 'solar_system',
+					'Near a star' => 'star',
+					'Interstellar Space' => 'interstellar_space'
+				],
+				'required' => false
+			])
+			->add('specificLocationName', ChoiceType::class, [
+				'placeholder' => 'Where Exactly?',
+				'choices' => [
+					'TODO' => 'TODO'
+				],
+				'required' => false
+			])
+		;
+		if ($options['include_published_at']){
+			$builder
+				->add('publishedAt', null, [
+				'widget' => 'single_text'
+				]);
+		}
+	}
+
+	public function configureOptions(OptionsResolver $resolver) {
+		$resolver->setDefaults([
+			'data_class' => Article::class,
+			'include_published_at' => false
+		]);
+	}
+
+
+}
